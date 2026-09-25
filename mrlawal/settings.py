7fcfea31b2e004ae19811bbc732f1bcd6e3313c4
BELLO AKEEM <belloakeem123@gmail.com>
2026-09-25 03:10:01 +0100

    Update accounting application

--- mrlawal/settings.py
+++ mrlawal/settings.py
@@ -114,8 +114,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
+
+# Only needed in development
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
